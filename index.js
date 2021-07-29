516f5d6ac09e54abc5e2c640f00261033f00cedb
vanceleon <vanceleon44@gmail.com>
2021-07-29 11:17:56 -0400

    routes

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// http://localhost:8000/
+app.post('/create', (req, res) => {
+  res.send('Create Reminder');
+});
+
+app.listen(port, () => {
+  console.log(`====Example app listening at http://localhost:${port}=====`);
+});
