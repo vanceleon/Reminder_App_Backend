683557ccfb97da75a439ad7e52938aabd20f2599
vanceleon <vanceleon44@gmail.com>
2021-08-03 08:17:58 -0400

    get request built with dummydata

--- index.js
+++ index.js
@@ -1,25 +1,37 @@
 const express = require('express');
 const app = express();
-const reminders = require('./listOfReminders/listOfRemindersArr')
-// import { listOfRemindersArr } from './listOfReminders';
+const faker = require('faker');
+const _ = require('lodash');
 const port = 8000;
 
+const dummmyReminders = faker.lorem.text();
 
-console.log("reminders",reminders)
-app.get("/reminders", (req, res) => {
-  listOfRemindersArr
-    .get()
-    .then(reminders => {
-      if (reminders) {
-        res.status(200).json(reminders);
-      } else {
-        res.status(404).json({ message: "No reminder found" });
-      }
-    })
-    .catch(err => {
-      console.log("error", err);
-      res.status(500).json({ messgae: "Error getting the information" });
-    });
+app.get('/', (req, res) => {
+  const count = 1;
+  if (!count) {
+    console.log('in 400 error')
+    return res
+      .status(400)
+      .send({ errorMsg: 'count query parameter is missing' });
+  } else if (dummmyReminders) {
+    console.log('in 200 ')
+    // const dummmyReminders = faker.lorem.text();
+    const uuid = faker.datatype.uuid();
+
+    const reminderObject = {
+      id: uuid,
+      reminder: dummmyReminders,
+    };
+    console.log("in reminders object")
+    console.log(reminderObject)
+    res.status(200).json(reminderObject);
+  } else {
+    console.log('in 500 error')
+    err => {
+      console.log('error', err);
+      res.status(500).json({ messgae: 'Error getting the information' });
+    };
+  }
 });
 
 app.listen(port, () => {
